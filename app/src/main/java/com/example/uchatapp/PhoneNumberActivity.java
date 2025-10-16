9086007896e9163b04b4f06feee1fb4f805c8b09
aman <aman.salimali@gmail.com>
2025-10-16 17:06:14 +0500

    -OTP verification and User creation done through phone number.

--- app/src/main/java/com/example/uchatapp/PhoneNumberActivity.java
+++ app/src/main/java/com/example/uchatapp/PhoneNumberActivity.java
@@ -1,24 +1,23 @@
 package com.example.uchatapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.example.uchatapp.databinding.ActivityPhoneNumberBinding;
 
 public class PhoneNumberActivity extends AppCompatActivity {
 
+    ActivityPhoneNumberBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_phone_number);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.continueBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(PhoneNumberActivity.this,OTPActivity.class);
+            intent.putExtra("phoneNumber",binding.phoneBox.getText().toString());
+            startActivity(intent);
         });
     }
 }
