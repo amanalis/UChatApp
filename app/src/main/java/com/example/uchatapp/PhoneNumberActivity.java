9b203064eb41d6ec65011592f2b05440894977d1
aman <aman.salimali@gmail.com>
2025-10-17 13:23:41 +0500

    -Validations added for number to pass.

--- app/src/main/java/com/example/uchatapp/PhoneNumberActivity.java
+++ app/src/main/java/com/example/uchatapp/PhoneNumberActivity.java
@@ -2,6 +2,8 @@ package com.example.uchatapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.uchatapp.databinding.ActivityPhoneNumberBinding;
 
@@ -15,9 +17,21 @@ public class PhoneNumberActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.continueBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(PhoneNumberActivity.this,OTPActivity.class);
-            intent.putExtra("phoneNumber",binding.phoneBox.getText().toString());
-            startActivity(intent);
+            String phoneNumber = binding.phoneBox.getText().toString().trim();
+            if(phoneNumber.isEmpty()){
+                Toast.makeText(this, "Phone Number cannot be empty!", Toast.LENGTH_SHORT).show();
+            }
+            else if(!phoneNumber.startsWith("+92")){
+                Toast.makeText(this, "Phone number should start with +92", Toast.LENGTH_SHORT).show();
+            }
+            else if(phoneNumber.length()!= 13){
+                Toast.makeText(this, "Invalid phone number. Must be +92 followed by 10 digits", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Intent intent = new Intent(PhoneNumberActivity.this,OTPActivity.class);
+                intent.putExtra("phoneNumber",binding.phoneBox.getText().toString());
+                startActivity(intent);
+            }
         });
     }
 }
