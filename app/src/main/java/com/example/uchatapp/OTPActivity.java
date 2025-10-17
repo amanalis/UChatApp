814891c63ffda751dce43b71c07a49c0feda2aa9
aman <aman.salimali@gmail.com>
2025-10-17 13:28:45 +0500

    -added timer and resend option for getting otp again.

--- app/src/main/java/com/example/uchatapp/OTPActivity.java
+++ app/src/main/java/com/example/uchatapp/OTPActivity.java
@@ -7,7 +7,6 @@ import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
-import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
