9e4e7e8749bd40aec808a8658129fe93e284d94e
aman <aman.salimali@gmail.com>
2025-10-23 17:08:53 +0500

    -Reactions done, saved in database showed along side messages.

--- app/src/main/java/com/example/uchatapp/Adapters/MessagesAdapter.java
+++ app/src/main/java/com/example/uchatapp/Adapters/MessagesAdapter.java
@@ -2,7 +2,6 @@ package com.example.uchatapp.Adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
