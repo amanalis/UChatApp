316e6757b339028cedd3c81609dde37451772a20
aman <aman.salimali@gmail.com>
2025-10-24 16:46:39 +0500

    -User Status loading from DB.

--- app/src/main/java/com/example/uchatapp/Adapters/TopStatusAdapter.java
+++ app/src/main/java/com/example/uchatapp/Adapters/TopStatusAdapter.java
@@ -8,11 +8,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.uchatapp.Activities.MainActivity;
+import com.example.uchatapp.Models.Status;
 import com.example.uchatapp.Models.UserStatus;
 import com.example.uchatapp.R;
 import com.example.uchatapp.databinding.ItemStatusBinding;
 
 import java.util.ArrayList;
+
+import omari.hamza.storyview.StoryView;
+import omari.hamza.storyview.callback.StoryClickListeners;
+import omari.hamza.storyview.model.MyStory;
 
 public class TopStatusAdapter extends RecyclerView.Adapter<TopStatusAdapter.TopStatusViewHolder> {
 
@@ -33,7 +40,42 @@ public class TopStatusAdapter extends RecyclerView.Adapter<TopStatusAdapter.TopS
 
     @Override
     public void onBindViewHolder(@NonNull TopStatusViewHolder holder, int position) {
+        UserStatus userStatus = userStatuses.get(position);
 
+        //UserStatus userStatus = userStatuses.get(position)
+        Status lastStatus = userStatus.getStatuses().get(userStatus.getStatuses().size()-1);
+
+        Glide.with(context).load(lastStatus.getImageUrl()).into(holder.binding.image);
+
+        holder.binding.circularStatusView.setPortionsCount(userStatus.getStatuses().size());
+
+
+        holder.binding.circularStatusView.setOnClickListener(v -> {
+            ArrayList<MyStory> myStories = new ArrayList<>();
+            for (Status status:userStatus.getStatuses()){
+                myStories.add(new MyStory(status.getImageUrl()));
+            }
+
+            new StoryView.Builder(((MainActivity)context).getSupportFragmentManager())
+                    .setStoriesList(myStories) // Required
+                    .setStoryDuration(5000) // Default is 2000 Millis (2 Seconds)
+                    .setTitleText(userStatus.getName()) // Default is Hidden
+                    .setSubtitleText("") // Default is Hidden
+                    .setTitleLogoUrl(userStatus.getProfileImage()) // Default is Hidden
+                    .setStoryClickListeners(new StoryClickListeners() {
+                        @Override
+                        public void onDescriptionClickListener(int position1) {
+                            //your action
+                        }
+
+                        @Override
+                        public void onTitleIconClickListener(int position1) {
+                            //your action
+                        }
+                    }) // Optional Listeners
+                    .build() // Must be called before calling show method
+                    .show();
+        });
     }
 
     @Override
