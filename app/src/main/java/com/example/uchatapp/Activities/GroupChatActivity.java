107a69704938099c62f6e38c6f5da480f7cd527d
aman <aman.salimali@gmail.com>
2025-10-30 17:04:12 +0500

    -Sending Images in Group Chat.

--- app/src/main/java/com/example/uchatapp/Activities/GroupChatActivity.java
+++ app/src/main/java/com/example/uchatapp/Activities/GroupChatActivity.java
@@ -1,25 +1,33 @@
 package com.example.uchatapp.Activities;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.uchatapp.Adapters.GroupMessagesAdapter;
 import com.example.uchatapp.Models.Message;
 import com.example.uchatapp.databinding.ActivityGroupChatBinding;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
 public class GroupChatActivity extends AppCompatActivity {
 
@@ -88,6 +96,52 @@ public class GroupChatActivity extends AppCompatActivity {
                         .setValue(message);
             }
         });
+
+        binding.attachment.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+//                "images/*" = for images
+//                "video/*" = for videos
+//                "*/*" = for everything
+            intent.setType("image/*");
+            startActivityForResult(intent, 25);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 25) {
+            if (data != null) {
+                if (data.getData() != null) {
+                    Uri selectedImage = data.getData();
+                    Calendar calendar = Calendar.getInstance();
+                    StorageReference reference = storage.getReference()
+                            .child("chats")
+                            .child(calendar.getTimeInMillis() + "");
+                    dialog.show();
+                    reference.putFile(selectedImage).addOnCompleteListener((Task<UploadTask.TaskSnapshot> task) -> {
+                        dialog.dismiss();
+                        if (task.isSuccessful()) {
+                            reference.getDownloadUrl().addOnSuccessListener((Uri uri) -> {
+                                String filePath = uri.toString();
+
+                                String messageTxt = binding.messageBox.getText().toString();
+
+                                Date date = new Date();
+                                Message message = new Message(messageTxt, senderUid, date.getTime());
+                                message.setMessage("photo");
+                                message.setImageUrl(filePath);
+                                binding.messageBox.setText("");
+
+                                database.getReference().child("public").push().setValue(message);
+                            });
+                        }
+                    });
+                }
+            }
+        }
     }
 
     @Override
