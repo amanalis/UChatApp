4eaa49bb49984976f016c4ff3c60b7fa6cd531cc
aman <aman.salimali@gmail.com>
2025-10-30 15:58:46 +0500

    -Adapter made for GroupChat .

---
+++ app/src/main/java/com/example/uchatapp/Adapters/GroupMessagesAdapter.java
@@ -0,0 +1,176 @@
+package com.example.uchatapp.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.uchatapp.Models.Message;
+import com.example.uchatapp.R;
+import com.example.uchatapp.databinding.ItemRecieveBinding;
+import com.example.uchatapp.databinding.ItemSendBinding;
+import com.github.pgreze.reactions.ReactionPopup;
+import com.github.pgreze.reactions.ReactionsConfig;
+import com.github.pgreze.reactions.ReactionsConfigBuilder;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class GroupMessagesAdapter extends RecyclerView.Adapter{
+
+    Context context;
+    ArrayList<Message> messages;
+
+    final int ITEM_SENT = 1;
+    final int ITEM_RECEIVE = 2;
+
+
+    public GroupMessagesAdapter(Context context, ArrayList<Message> messages) {
+        this.context = context;
+        this.messages = messages;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Message message = messages.get(position);
+        if (FirebaseAuth.getInstance().getUid().equals(message.getSenderId())){
+            return ITEM_SENT;
+        } else {
+            return ITEM_RECEIVE;
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == ITEM_SENT){
+            View view = LayoutInflater.from(context).inflate(R.layout.item_send,parent,false);
+            return new SentViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(context).inflate(R.layout.item_recieve,parent,false);
+            return new ReceiveViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Message message = messages.get(position);
+
+        int[] reactions = new int[]{
+                R.drawable.ic_fb_like,
+                R.drawable.ic_fb_love,
+                R.drawable.ic_fb_laugh,
+                R.drawable.ic_fb_wow,
+                R.drawable.ic_fb_sad,
+                R.drawable.ic_fb_angry
+        };
+
+        ReactionsConfig config = new ReactionsConfigBuilder(context)
+                .withReactions(reactions)
+                .build();
+
+        ReactionPopup popup = new ReactionPopup(context, config, (pos) -> {
+            if (holder.getClass() == SentViewHolder.class){
+                SentViewHolder viewHolder = (SentViewHolder) holder;
+                viewHolder.binding.feeling.setImageResource(reactions[pos]);
+                viewHolder.binding.feeling.setVisibility(View.VISIBLE);
+            }else{
+                ReceiveViewHolder viewHolder = (ReceiveViewHolder) holder;
+                viewHolder.binding.feeling.setImageResource(reactions[pos]);
+                viewHolder.binding.feeling.setVisibility(View.VISIBLE);
+            }
+            message.setFeeling(pos);
+
+            FirebaseDatabase.getInstance().getReference()
+                    .child("public")
+                    .child(message.getMessageId()).setValue(message);
+
+            FirebaseDatabase.getInstance().getReference()
+                    .child("chats")
+                    .child(message.getMessageId()).setValue(message);
+            return true; // true is closing popup, false is requesting a new selection
+        });
+
+        if (holder.getClass() == SentViewHolder.class){
+            SentViewHolder viewHolder = (SentViewHolder) holder;
+
+            if (message.getMessage().equals("photo")){
+                viewHolder.binding.image.setVisibility(View.VISIBLE);
+                viewHolder.binding.message.setVisibility(View.GONE);
+                Glide.with(context).load(message.getImageUrl()).placeholder(R.drawable.placeholder).into(viewHolder.binding.image);
+            }
+
+            viewHolder.binding.message.setText(message.getMessage());
+
+            if(message.getFeeling()>=0){
+//                message.setFeeling(reactions[(int) message.getFeeling()]);
+                viewHolder.binding.feeling.setImageResource(reactions[(int) message.getFeeling()]);
+                viewHolder.binding.feeling.setVisibility(View.VISIBLE);
+            } else {
+                viewHolder.binding.feeling.setVisibility(View.GONE);
+            }
+
+            viewHolder.binding.message.setOnTouchListener((view, event) -> {
+                popup.onTouch(view,event);
+                return false;
+            });
+
+            viewHolder.binding.image.setOnTouchListener((view, event) -> {
+                popup.onTouch(view,event);
+                return false;
+            });
+        } else {
+            ReceiveViewHolder viewHolder = (ReceiveViewHolder) holder;
+            if (message.getMessage().equals("photo")){
+                viewHolder.binding.image.setVisibility(View.VISIBLE);
+                viewHolder.binding.message.setVisibility(View.GONE);
+                Glide.with(context).load(message.getImageUrl()).placeholder(R.drawable.placeholder).into(viewHolder.binding.image);
+            }
+            viewHolder.binding.message.setText(message.getMessage());
+
+            if(message.getFeeling()>=0){
+                viewHolder.binding.feeling.setImageResource(reactions[(int) message.getFeeling()]);
+                viewHolder.binding.feeling.setVisibility(View.VISIBLE);
+            } else {
+                viewHolder.binding.feeling.setVisibility(View.GONE);
+            }
+
+            viewHolder.binding.message.setOnTouchListener((view, event) -> {
+                popup.onTouch(view,event);
+                return false;
+            });
+
+            viewHolder.binding.image.setOnTouchListener((view, event) -> {
+                popup.onTouch(view,event);
+                return false;
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    public class SentViewHolder extends RecyclerView.ViewHolder{
+
+        ItemSendBinding binding;
+        public SentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemSendBinding.bind(itemView);
+        }
+    }
+
+    public class ReceiveViewHolder extends RecyclerView.ViewHolder{
+        ItemRecieveBinding binding;
+        public ReceiveViewHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = ItemRecieveBinding.bind(itemView);
+        }
+    }
+}
